2f153f917695c903d97d174bf0390875bce95ac8
James Anuncio <anunciojames14@gmail.com>
2023-01-09 21:10:21 +0800

    Aasd

--- components/About.js
+++ components/About.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 
-import Picture from "../public/assets/Picture.PNG";
+import Picture from "../public/assets/Picture.png";
 
 const About = () => {
   return (
